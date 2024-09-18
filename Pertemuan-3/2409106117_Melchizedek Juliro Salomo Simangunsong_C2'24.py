1b5c50900928841763424a4d95f7a4c2ecdfa804
Melchi050706 <melchisimangunsong9@gmail.com>
2024-09-18 16:16:38 +0800

    Add files via upload

---
+++ Pertemuan-3/2409106117_Melchizedek Juliro Salomo Simangunsong_C2'24.py
@@ -0,0 +1,49 @@
+# cuaca = "mendung"
+
+# if cuaca == "mendung":
+#     print("diam di rumah")
+# else:
+#     print("Hari ini mendung")
+
+# Umur = int(input("Masukkan umur Anda : "))
+# if Umur > 0:
+#     if Umur <= 10: 
+#         print("Umur Anda termasuk kategori anak-anak yang masih bayi")
+#     elif Umur <= 18:
+#         print("Umur Anda termasuk kategori anak-anak yang masih remaja")
+#     elif Umur <=35:
+#         print("Umur Anda termasuk kategori dewasa muda")
+#     elif Umur <=65:
+#         print("Umur Anda termasuk kategori dewasa tua")
+#     else:
+#         print("Umur Anda termasuk kategori Tua")
+# else:
+#     print("input usia harus bilangan positif")
+
+# nim = int(input("masukkan nim: "))
+
+# if nim >= 1 and nim <= 50 :
+#     if nim >= 1 and nim <= 25 :
+#         print("kelas A'1")
+#     else :
+#         print("kelas A'2")
+# elif nim >= 51 and nim <= 100 :
+#     if nim >= 51 and nim <= 75 :
+#         print("kelas B'1")
+#     else :
+#         print("kelas B'2")
+# elif nim >= 101 and nim <= 121 :
+#     if nim >= 101 and nim <= 110 :
+#         print("kelas C'1")
+#     else :
+#         print("kelas C'2")
+# else :
+#     print("anda bukan mahasiswa informatika")
+
+# angka = int(input("Masukkan angka: "))
+# result = "Genap" if angka % 2 == 0 else "Ganjil"
+# print(angka, "adalah angka",result)
+
+# nim = int(input("Masukkan nim"))
+# hasil = "Kelas A" if nim >= 1 and nim <=50 else "Kelas B" if nim >= 51 and nim <= 100 else "Kelas c" if nim >= 101 and nim <= 121 else "mahasiswa goib" 
+# print(hasil)
