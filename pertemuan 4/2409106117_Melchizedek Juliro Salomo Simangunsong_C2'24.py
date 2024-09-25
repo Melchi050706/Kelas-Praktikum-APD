6bd4ee2acbd351804451564fa12ad00f8632d268
2409106117_Melchizedek Juliro Salomo Simangunsong <melchisimangunsong9@gmail.com>
2024-09-25 16:04:35 +0800

    Add files via upload

---
+++ pertemuan 4/2409106117_Melchizedek Juliro Salomo Simangunsong_C2'24.py
@@ -0,0 +1,82 @@
+# ulang = 10
+# for i in range(ulang):
+#         print("Perulangan ke-" + str(i))
+
+# for i in range(2,10,):
+#     print(f"Perulangan ke-" + str(i))
+
+# for i in range(2,10,2):
+#     print(f"Perulangan ke-" + str(i))
+  
+# for i in range(10,2,-1):
+#     print(f"Perulangan ke-" + str(i))
+
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+#     print(f"Total perulangan: {hitung}")
+
+# while True:
+#     print("halo world")
+
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(1,12):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# print("Daftar bilangan genap dari 1-10")
+# for i in range(1,11):
+#     if i % 2 != 0:
+#         continue
+#     print(i)
+
+# kelasC = ["melchi", 2409106117]
+
+# for i in range(len(kelasC)):
+#     print(f"{i+1} {kelasC[i]}")
+
+
+
+# while True:
+#     print("halo")
+#     print("fufufafa")
+#     continue
+
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         break
+
+# print(f"Total Perulangan: {hitung}")
+
+# while True:
+#     print("halo")
+#     break
+
+
+
