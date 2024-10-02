92de7ce07c8b5bdd096f2ef9ab439686c9ca601d
2409106117_Melchizedek Juliro Salomo Simangunsong <melchisimangunsong9@gmail.com>
2024-10-02 16:46:06 +0800

    Add files via upload

---
+++ pertemuan 5/2409106117_Melchizedek Juliro Salomo Siamangunsong_C2'24.py
@@ -0,0 +1,72 @@
+# nama = ["shandy, 106, True, 3.96"]
+# matkul = ["APD","APL", "WEB", "JARKOM", "BASDAT", "STRUKDAT", "PTI", "KALKULUS", "fisika"]
+# ]
+# print(nama)
+# print(matkul[1:8])
+
+# nama = ["shandy, 106, True,["YUYUN", 145] 3.96", [123, "ALVITO", False, 3.66], "REHAN"]
+
+# matkul = ["APD",
+#           "APL",
+#           "WEB", 
+#           "JARKOM",
+#           "BASDAT",
+#           "STRUKDAT"
+#           "PTI",
+#           "KALKULUS"
+# ]
+# print(nama[4])
+# print(matkul[3])
+
+# MAKANAN = ["Bakso", "Sate", "Soto"]
+# MAKANAN.append()"Nasi Goreng"]
+# print(MAKANAN)
+
+# MAKANAN = ["Bakso", "Sate", "Soto"]
+# print(MAKANAN)
+# MAKANAN.insert(2, "Nasi Goreng")
+# print(MAKANAN)
+
+# MAKANAN = ["Bakso", "Sate", "Soto"]
+# del MAKANAN [0]
+# print(MAKANAN)
+
+# MAKANAN = ["Bakso", "Sate", "Soto"]
+# data = MAKANAN.pop(1)
+# print(MAKANAN)
+# print(data)
+
+# MAKANAN = ["Bakso", "Sate", "Soto"]
+# MAKANAN.clear()
+# print(MAKANAN)
+
+# minuman = ["Pocari", "Teh pusuk", "es teh", "es jeruk", "es kelapa", "soda", "teh gelas", "kratingdeng"]
+
+# print(minuman)
+
+# del minuman[3]
+# minuman[6] = "air putih"
+# minuman.insert(0, "jus tomat")
+
+# print(minuman)
+
+
+# benda = ["kalung", "cincin", "baju", "celana", "kacamata", ]
+
+# makanan = ["ayam", "ikan",["bakso", "soto", "sate", "ikan", "ayam"],['teh', "kopi", "air"]]
+# for i in makanan :
+#     if isinstance(i, list):
+#         for j in i :
+#             print(j)
+#     else:
+#         print(i)
+
+    # for i in makanan :
+    #     for j in i :
+    #         print(j,)
+
+
+
+
+# TUPLE
+
