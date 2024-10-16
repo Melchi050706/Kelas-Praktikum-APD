878e164def6b15e92226f658a2731dae938c6058
2409106117_Melchizedek Juliro Salomo Simangunsong <melchisimangunsong9@gmail.com>
2024-10-16 16:16:03 +0800

    Add files via upload

---
+++ Pertemuan 7/2409106117_Melchizedek Juliro Salomo Simangunsong_C2'24.py
@@ -0,0 +1,61 @@
+# def mhs():
+#     print("Helloakt")
+# mhs()
+# mhs()
+
+# def kali():
+#     x = 6*4
+#     print(x)
+
+# def luas_persegi_panjang(panjang, lebar):
+#     luas = panjang * lebar
+#     print ("Luas persegi panjang:", luas)
+
+# luas_persegi_panjang(4,6)
+
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     keliling = 4*sisi
+#     return luas, keliling
+# hasil = luas_persegi(4) 
+# print(hasil)
+
+
+# def mhs(nama, nim, *arr):
+#     print(nama)
+#     print(nim)
+#     print(arr)
+# mhs("ucup", "20", 1,2,3,4,5)
+
+# def mhs(nama, nim, **arr):
+#     print(nama)
+#     print(nim)
+#     print(arr)
+#     return
+# mhs("ucup", "20", data = 2, data2=3)
+
+# def nilai(angka):
+#     if angka >10:
+#         print("1")
+#         # return
+#     else:
+#         print("2")
+#         # return
+#     print("3")
+
+# nilai(10)
+
+# data = 10
+
+# def nilai():
+#     data = 20
+#     print(data)
+
+# nilai()
+# print(data)
+
+def data(kumpulan):
+    print(kumpulan)
+
+value = [1,2,3,4]
+# data(value)
